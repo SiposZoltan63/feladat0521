e24b7630ee55e42b3203d4eb4051bedc19f5df30
SiposZoltan63 <siposz@kkszki.hu>
2025-05-21 09:09:56 +0200

    Add project files.

---
+++ feladat0521/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace feladat0521
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Termek> termekek = new List<Termek>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // 1. beolvasuk a keszlet.csv-t
+            var fajl = File.ReadAllLines("keszlet.csv");
+            foreach (var item in fajl)
+            {
+                var sor = item.Split(';');
+                var termek = new Termek(sor[0], sor[1], int.Parse(sor[2]), int.Parse(sor[3]));
+                termekek.Add(termek);
+            }
+            // 2. feltöltjük a DataGrid-et
+            dgkeszlet.ItemsSource = termekek;
+        }
+
+        private void dgkeszlet_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            txCikkszam.Text = ((Termek)dgkeszlet.SelectedItem).Cikkszam;
+            txMegnevezes.Text = ((Termek)dgkeszlet.SelectedItem).Megnevezes;
+            txAr.Text = ((Termek)dgkeszlet.SelectedItem).Ar.ToString();
+            txMennyiseg.Text = ((Termek)dgkeszlet.SelectedItem).Mennyiseg.ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ feladat0521/Termek.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feladat0521
+{
+    internal class Termek
+    {
+        public string Cikkszam { get; set; }
+        public string Megnevezes { get; set; }
+        public int Ar { get; set; }
+        public int Mennyiseg { get; set; }
+
+        public Termek(string cikkszam, string megnevezes, int ar, int mennyiseg)
+        {
+            Cikkszam = cikkszam;
+            Megnevezes = megnevezes;
+            Ar = ar;
+            Mennyiseg = mennyiseg;
+        }
+    }
+}
